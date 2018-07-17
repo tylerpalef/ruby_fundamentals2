06f869b2ac7af70e87d96351e2456d55c1bfea40
“Tyler <tylerpalef@gmail.com>
2018-07-17 13:22:41 -0400

    negative

--- exercise1.rb
+++ exercise1.rb
@@ -1,7 +1,18 @@
-def double(my_number)
-  return "#{my_number.to_f*2}"
+# def double(my_number)
+#   return "#{my_number.to_f*2}"
+# end
+#
+# # puts double("5")
+#
+# puts double("15")
+
+def negative(num)
+  if negative >= 0
+    return negative == true
+
+  else
+    negative < 0
+    return negative == false
 end
 
-# puts double("5")
-
-puts double("15")
+puts negative(10)
