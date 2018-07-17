f44a10dbee5f8e961f3a98ec057fb9c7703379a0
“Tyler <tylerpalef@gmail.com>
2018-07-17 13:59:16 -0400

    finished exercise 5

--- exercise1.rb
+++ exercise1.rb
@@ -1,58 +1,77 @@
 # Exercise 1
+#Create a number doubler
 #*********************************
 
-# def double(my_number)
-#   return "#{my_number.to_f*2}"
-# end
-#
-# # puts double("5")
-#
-# puts double("15")
+    # def double(my_number)
+    #   return "#{my_number.to_f*2}"
+    # end
+    #
+    # # puts double("5")
+    #
+    # puts double("15")
 
 #Exercise 2
+#Negative numbers return with "false"
 #*********************************
-# def negative? (num)
-#   if num < 0
-#     return true
-#
-#   elsif
-#     num > 0
-#     return false
-# end
-# end
-# puts negative?(55)
+
+    # def negative? (num)
+    #   if num < 0
+    #     return true
+    #
+    #   elsif
+    #     num > 0
+    #     return false
+    # end
+    # end
+    # puts negative?(55)
 
 # Exercise 3
+#Odd numbers return as false
 #*********************************
 
-# def is_even?(num)
-#
-#   if num % 2 == 0
-#     return true
-#   else
-#     return false
-#   end
-# end
-#
-# puts is_even?(69)
+    # def is_even?(num)
+    #
+    #   if num % 2 == 0
+    #     return true
+    #   else
+    #     return false
+    #   end
+    # end
+    #
+    # puts is_even?(69)
 
 # Exercise 4
+# Names more than 8 characters are considered true
 #*********************************
-def name_eight(name)
-  name = name.to_s
-  if name.length < 8
-    return false
-  else
-    true
-  end
-end
 
-puts name_eight("testingtester")
+    # def name_eight(name)
+    #   name = name.to_s
+    #   if name.length < 8
+    #     return false
+    #   else
+    #     true
+    #   end
+    # end
+    #
+    # puts name_eight("testingtester")
 
 # Exercise 5
+#Take your name and reverse it
 #*********************************
 
+    # def backward_name(your_name)
+    #   return "hello #{your_name}"
+    # end
+    #
+    # puts "what is your name"
+    # your_name = gets.chomp.reverse
+    #
+    # puts your_name
+
 # Exercise 6
+#*********************************
+
+# Exercise 7
 #*********************************
 
 # Exercise 7
