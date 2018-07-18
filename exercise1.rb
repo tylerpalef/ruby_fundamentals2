8ef1912b7fc3795747f19bc6349b61a65e689731
“Tyler <tylerpalef@gmail.com>
2018-07-18 16:02:58 -0400

    finished exercise 8

--- exercise1.rb
+++ exercise1.rb
@@ -28,7 +28,7 @@
 # Exercise 3
 #Odd numbers return as false
 #*********************************
-
+    #
     # def is_even?(num)
     #
     #   if num % 2 == 0
@@ -101,16 +101,86 @@
 #Wraps extra spaces for text
 #*********************************
 
-    # def wrap_text(text)
-    #   return "     " + "#{text}" + "     "
+    # def wrap_text(text, space)
+    #   return space + text + space
     # end
     #
-    # puts wrap_text("adadadadadadad")
-    # puts wrap_text("lmao i did it")
-    #
+    # puts wrap_text("adadadadadadad", "lol")
+    # # puts wrap_text("lmao i did it")
+
     # puts "give me a word to add spaces to"
     # text = gets.chomp.to_s.upcase
     # puts wrap_text(text)
 
-# Exercise 7
+# Exercise 8
 #*********************************
+
+def speed(person)
+  puts "How far did #{person} run (in metres)?"
+  distance = gets.to_f
+  # puts distance
+  # Confirming distance
+  puts "How long (in minutes) did #{person} take to run #{distance} metres"
+  mins = gets.to_f
+  # puts mins
+  speed = distance / mins.to_f
+  return speed
+  # Confirming min
+end
+
+speed1 = speed("person_1")
+speed2 = speed("person_2")
+speed3 = speed("person_3")
+
+puts speed1
+puts speed2
+puts speed3
+
+if speed3 > speed2 && speed3 > speed1
+  puts "Person 3 was the fastest at #{speed3} m/s"
+elsif speed2 > speed3 && speed2 > speed1
+  puts "Person 2 was the fastest at #{speed2} m/s"
+elsif speed1 > speed3 && speed1 > speed2
+  puts "Person 1 was the fastest at #{speed1} m/s"
+elsif speed1 == speed2 && speed2 == speed3
+  puts "Everyone tied at #{speed1} m/s"
+else
+  puts "Well done everyone!"
+end
+
+#
+# if speed3 > speed2 && speed3 > speed1
+#   puts "Person 3 was the fastest at #{speed3} m/s"
+# elsif speed2 > speed3 && speed2 > speed1
+#   puts "Person 2 was the fastest at #{speed2} m/s"
+# elsif speed1 > speed3 && speed1 > speed2
+#   puts "Person 1 was the fastest at #{speed1} m/s"
+# elsif speed1 == speed2 && speed2 == speed3
+#   puts "Everyone tied at #{speed1} m/s"
+# else
+#   puts "Well done everyone!"
+# end
+#
+#
+# # dist1, long1 = metres("Person 1")
+# puts dist1
+# puts long1
+# speed1 = dist * long1.to_f
+
+# puts metres("person_1")
+#
+# puts "How far did person 1 run (in metres)?"
+# distance1 = gets.to_f
+# puts "How long (in minutes) did person 1 run take to run #{distance1} metres?"
+# mins1 = gets.to_f
+#
+#
+
+#
+# secs1 = mins1 * 60
+# speed1 = distance1/secs1
+# secs2 = mins2 * 60
+# speed2 = distance2/secs2
+# secs3 = mins3 * 60
+# speed3 = distance3/secs3
+#
